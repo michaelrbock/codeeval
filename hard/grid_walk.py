32e9d95d77114644c0afa287fda86eb5241456ef
Michael Bock <mykel.bock@gmail.com>
2014-07-26 10:54:11 -0700

    Grid walk under 20MB used

--- hard/grid_walk.py
+++ hard/grid_walk.py
@@ -1,42 +1,39 @@
 import sys
-from collections import defaultdict, namedtuple
-
-Point = namedtuple('Point', 'x y')
 
 def is_accessible(point):
     total = 0
-    for digit in str(point.x).replace('-', ''):
+    for digit in str(point[0]).replace('-', ''):
         total += int(digit)
-    for digit in str(point.y).replace('-', ''):
+    for digit in str(point[1]).replace('-', ''):
         total += int(digit)
     return total <= 19
 
-visited = defaultdict(bool) # maps tuple of (x,y) to True/False
-to_vist = [Point(0, 0)] # points to explore
+visited = {} # maps tuple of (x,y) to True/False
+to_vist = [(0, 0)] # points to explore
 current = None # the current location in (x, y)
 count = 0 # the number of points accessible
 
 while to_vist:
     current = to_vist.pop(0)
-    if visited[current]:
+    if current in visited:
         continue
     count += 1
     visited[current] = True
     # UP
-    up = Point(current.x, current.y - 1)
-    if is_accessible(up) and not visited[up]:
+    up = (current[0], current[1] - 1)
+    if is_accessible(up) and up not in visited:
         to_vist.append(up)
     # DOWN
-    down = Point(current.x, current.y + 1)
-    if is_accessible(down) and not visited[down]:
+    down = (current[0], current[1] + 1)
+    if is_accessible(down) and down not in visited:
         to_vist.append(down)
     # LEFT
-    left = Point(current.x - 1, current.y)
-    if is_accessible(left) and not visited[left]:
+    left = (current[0] - 1, current[1])
+    if is_accessible(left) and left not in visited:
         to_vist.append(left)
     # RIGHT
-    right = Point(current.x + 1, current.y)
-    if is_accessible(right) and not visited[right]:
+    right = (current[0] + 1, current[1])
+    if is_accessible(right) and right not in visited:
         to_vist.append(right)
 
 sys.stdout.write(str(count))
